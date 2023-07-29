b205025e74a24fb66c4bb476b579a6e7943c8a77
Partha Sarathi Dey <parthasdeyofficial@gmail.com>
2023-07-29 03:51:36 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ function App() {
  return (
     <div>
       <Navbar />
-      <div className='w-full h-screen p-4 md:px-64 md:py-32'>
-        <div className='w-full h-full border-2 md:flex md:space-x-5 p-6 rounded-3xl'>
-          <div className='w-full h-full border-2 rounded-3xl p-6'>
-            <div className='rounded-3xl w-full h-full p-6 py-32 flex-col'>
-              <h1 className='text-6xl font-semibold'>I'm Partha Sarathi Dey</h1>
+      <div className='w-full h-screen md:h-[1300px] p-4 md:px-64 md:py-20'>
+        <div className='w-full h-full md:flex md:space-x-5 p-6 rounded-3xl'>
+          <div className='w-full h-full rounded-3xl p-6'>
+            <div className='rounded-3xl w-full h-full p-10 md:py-80 flex-col space-y-10'>
+              <h1 className='text-5xl md:text-6xl font-semibold text-blue-600'>I'm <br /> Partha Sarathi Dey</h1>
+              <p className='text-xl md:text-3xl font-semibold md:font-medium'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur voluptatibus repudiandae dignissimos temporibus eius harum sunt, non in ut assumenda quaerat ad cumque, animi ullam itaque magni dicta id neque!</p>
             </div>
           </div>
-          <div className='w-full h-full border-2 rounded-3xl p-6'>
-            <div className='border-2 rounded-3xl w-full h-full p-6'>
-
+          <div className='w-full h-full rounded-3xl'>
+            <div className='rounded-3xl w-full h-full md:pt-64 md:pl-64'>
+              <div className='w-80 h-80 md:w-[500px] md:h-[500px] rounded-full bg-black z-50'></div>
             </div>
           </div>
         </div>
       </div>
+
+      <div className='w-full h-[700px] bg-blue-600 -z-50'></div>
     </div>
   );
 }
